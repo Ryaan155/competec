5197a775f9cd1fb616827c8fe3c6ee34e4dc15b2
Ryaan155 <rs4977846@gmail.com>
2025-04-26 11:23:05 -0300

    Update Main.java

--- aula-0/Main.java
+++ aula-0/Main.java
@@ -1,7 +1,47 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+import java.util.Scanner;
 public class Main
 {
 	public static void main(String[] args) {
-        // Escreva seu código aqui ⬇️
-		System.out.println("Hello World");
+//atvd 1    
+char Nome='R';
+int Idade=15;
+boolean Cafe=true;
+	    
+//atvd2 
+
+// int laranja=15;
+//int uva=4;
+//int soma=uva+laranja;
+
+
+		   
+	//atvd 4   
+		   
+		   
+		Scanner entrada = new Scanner(System.in);
+		
+		System.out.println("Digite o valor da laranja: ");
+		int laranja = entrada.nextInt();
+		
+		System.out.println("Digite o valor da uva: ");
+		int uva = entrada.nextInt();	
+		
+		System.out.println("O resultado final da soma é = "+(laranja + uva) );
+		
+		
+		 
+		
+	
+		
+		
 	}
 }
+
+
